12ebb47bb60c5a7190f9c79632ffc35159750b6b
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-16 12:02:32 -0300

    atualizando o menu do painel

--- app/menu/menu.rb
+++ app/menu/menu.rb
@@ -1,7 +1,12 @@
 def menu()
     menu = "
-    [ 01 ] Cep
-    [ 02 ] Ip
+    l---------------------------l
+    l \033[1;31mchoose one of the options\033[m l
+    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    l        [ 01 ] Cep         l
+    l        [ 02 ] Ip          l
+    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
     "
     puts menu
 end
