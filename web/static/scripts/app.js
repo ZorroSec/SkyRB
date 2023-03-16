69e91403918f10984b6f8929aebefa21f1d90ca0
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-16 19:35:18 -0300

    eu literalmente no sei mais oque comitar

--- web/static/scripts/app.js
+++ web/static/scripts/app.js
@@ -1,1 +1,16 @@
-console.log('initialized')
+const timezone = ()=>{
+    const today = new Date()
+    const h = today.getHours()
+    const m = today.getMinutes()
+    const s = today.getSeconds()
+    const minutes = checkTime(m)
+    const seconds = checkTime(s)
+    document.getElementById('getHours').innerHTML=`${h}:${minutes}:${seconds}`
+    t=setTimeout('timezone()', 500)
+}
+const checkTime = (i)=>{
+    if(i<=10){
+        return "0" + i
+    }
+    return i
+}
