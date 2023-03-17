010ef87bb6fb13df8d4a908a6fa1af868ff6a698
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-17 15:25:03 -0300

    é tem erros simgit add .

--- web/static/scripts/ipApi.js
+++ web/static/scripts/ipApi.js
@@ -1,10 +1,10 @@
-const cep = document.getElementById('cep')
+const cep = document.getElementById('cep').value
 const btn = document.getElementById('btn')
 const ret = document.getElementById('return')
 
 if(btn){
     btn.onclick = ()=>{
-        fetch(`https://viacep.com.br/ws/${cep}/json/`).then((res)=>{
+        fetch(`https://viacep.com.br/ws/${cep}/json/`, {mode:'no-cors'}).then((res)=>{
             res.json()
         }).then((data)=>{
             ret.innerHTML=`<i>${data.logradouro}</i>`
