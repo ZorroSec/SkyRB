9213807141f2c09fd760f5dbf613196c048a479c
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-17 18:16:03 -0300

    ainda nao estou conseguindo resolver o erro, já procurei no stackoverflow mas nada!

--- web/static/scripts/ipApi.js
+++ web/static/scripts/ipApi.js
@@ -4,10 +4,18 @@ const ret = document.getElementById('return')
 
 if(btn){
     btn.onclick = ()=>{
-        fetch(`https://viacep.com.br/ws/${cep}/json/`, {mode:'no-cors'}).then((res)=>{
-            res.json()
+        fetch("https://viacep.com.br/ws/"+cep+"/json/", {
+            "mode": 'no-cors',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true 
+        }).then((res)=>{
+            return res.json()
+            res.set({
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
         }).then((data)=>{
-            ret.innerHTML=`<i>${data.logradouro}</i>`
+            ret.innerHTML=`<i>${data["logradouro"]}</i>`
         })
     }
 }
