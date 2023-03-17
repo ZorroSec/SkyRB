78c5f84eb0b3da0f4c649eb5ef287b696fee5340
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-17 20:26:13 -0300

    renomeando o arquivo ipAPi.js para cepApi.js pelo motive de eu ter escrevido todo o codigo de consuta cep no local erradogit status

---
+++ web/static/scripts/cepApi.js
@@ -0,0 +1,33 @@
+const cep = document.getElementById('cep')
+const btn = document.getElementById('btn')
+const ret = document.getElementById('return')
+
+// 01001000
+
+if(btn){
+    btn.onclick = ()=>{
+        fetch("https://viacep.com.br/ws/"+cep.value+"/json/").then((res)=>{
+            res.json().then((data)=>{
+                ret.innerHTML=`
+                <div class='returnBusca'>
+                    <div class='cep'>
+                        <code>
+                            Cep => ${data.cep}<br>
+                            Bairro => ${data.bairro}<br>
+                            Complemento => ${data.complemento}<br>
+                            Ddd => ${data.ddd}<br>
+                            Gia => ${data.gia}<br>
+                            Ibge => ${data.ibge}<br>
+                            Localidade => ${data.localidade}<br>
+                            Logradouro => ${data.logradouro}<br>
+                            Siafi => ${data.siafi}
+                            Uf => ${data.uf}
+                        </code>
+                    </div>
+                </div>
+                `
+                console.log(data)
+            })
+        })
+    }
+}
