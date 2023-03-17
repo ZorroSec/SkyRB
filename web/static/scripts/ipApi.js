6388abdfdae4e9f8f431af75d5db7d09727f0b6a
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-17 15:04:37 -0300

    removendo o import do js do index.html e removendo a funcao fetchUrl e levando para espera

--- web/static/scripts/ipApi.js
+++ web/static/scripts/ipApi.js
@@ -2,8 +2,6 @@ const fetchUrl = ()=>{
     fetch("http://ip-api.com/json/").then((res)=>{
         res.json()
     }).then((data)=>{
-        let ex = document.getElementById('ex')
-        ex.innerHTML=`<i>${data['query']}</i>`
+        return
     })
 }
-fetchUrl()
