2c6b46d515decb1a60ee8d070843f5ca55ed7989
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-18 14:01:19 -0300

    que erro do caralho

--- web/static/scripts/cnpjApi.js
+++ web/static/scripts/cnpjApi.js
@@ -1,1 +1,33 @@
-// https://www.receitaws.com.br/v1/cnpj/
+
+const cnpj = document.getElementById('cnpj')
+const btn = document.getElementById('btn')
+const ret = document.getElementById('return')
+
+if(btn){
+    btn.onclick = ()=>{
+        fetch(`https://www.receitaws.com.br/v1/cnpj/${cnpj.value}`, {
+            mode: 'no-cors',
+        }).then((res)=>{
+            res.json().then((data)=>{
+                // ret.innerHTML=`
+                // <div class='returnBusca'>
+                //     <div class='cnpj'>
+                //         <code>
+                //             data_sitacao => ${data.data_situacao}
+                //             fantasia => ${data.fantasia}
+                //             complemento => ${data.complemento}
+                //             tipo => ${data.tipo}
+                //             nome => ${data.nome}
+                //             efr => ${data.erf}
+                //             abertura => ${data.abertura}
+                //             telefone => ${data.telefone}
+                //             email => ${data.email}
+                //         </code>
+                //     </div>
+                // </div>
+                // `
+                console.log(data)
+            })
+        })
+    }
+}
