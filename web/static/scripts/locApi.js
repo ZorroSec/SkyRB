ccbd40152e171d3646ce22999e28579292f1ca23
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-18 21:51:01 -0300

    Tudo pronto em localidades.

--- web/static/scripts/locApi.js
+++ web/static/scripts/locApi.js
@@ -6,6 +6,18 @@ if(btn){{
     btn.onclick = ()=>{
         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf.value}`).then((res)=>{
             res.json().then((data)=>{
+                ret.innerHTML=`
+                <div class='returnBusca'>
+                    <div class='uf'>
+                        <code>
+                            id => ${data.id}
+                            sigla => ${data.sigla}
+                            nome => ${data.nome}
+                            regiao => ${data.regiao['nome']}
+                        </code>
+                    </div>
+                </div>
+                `
                 console.log(data)
             })
         })
