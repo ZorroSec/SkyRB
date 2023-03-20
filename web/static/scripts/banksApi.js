d05f1353978725d235780c45ccf299325edaac49
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-19 21:28:43 -0300

    fazendo o fetch em js

--- web/static/scripts/banksApi.js
+++ web/static/scripts/banksApi.js
@@ -1,3 +1,9 @@
 const ban = document.getElementById('bank')
 const btn = document.getElementById('btn')
 const ret = document.getElementById('return')
+
+if(btn){
+    btn.onclick = ()=>{
+        fetch(`https://brasilapi.com.br/api#tag/BANKS/paths/~1banks~1v1/get/banks/v1/${ban.value}`)
+    }
+}
