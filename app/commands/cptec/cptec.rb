ebb25089bb778316f5bdd0583896320eafef58af
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 18:57:22 -0300

    ****

--- app/commands/cptec/cptec.rb
+++ app/commands/cptec/cptec.rb
@@ -1,6 +1,8 @@
 require 'rest-client'
 require 'json'
 require_relative '../back.rb'
+require_relative 'cptec/city.rb'
+require_relative 'cptec/clima.rb'
 def menuCptec
 	menu = "
 	l---------------------------l
@@ -10,4 +12,12 @@ def menuCptec
 	>>> "
 	print menu
 	opc = gets.chomp.to_i
+	if opc == 1
+		system ('cls||clear')
+		cityName()
+	end
+	if opc == 2
+		system ('cls||clear')
+		clima()
+	end
 end
--- app/commands/cptec/cptecCmds/clima.rb
+++ app/commands/cptec/cptecCmds/clima.rb
@@ -1,1 +1,12 @@
-clima.rb
+require 'rest-client'
+require 'json'
+require_relative '../../back.rb'
+def clima
+	print 'CITY CODE > '
+	code = gets.chomp
+	req = RestClient.get "https://brasilapi.com.br/api/cptec/v1/clima/previsao/#{code}"
+	ret = JSON.parse(req.body)
+	ret.each do |key,value|
+		puts "#{key} => #{value}"
+	end
+end
