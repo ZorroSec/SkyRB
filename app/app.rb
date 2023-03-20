80f65e0a2d8090fa49238a74040a1cea66cc9b39
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 19:12:02 -0300

    the end?

--- app/app.rb
+++ app/app.rb
@@ -22,29 +22,29 @@ require 'json'
 while true
     menu()
     op = gets.chomp.to_i
-    if op == 1 || 01
+    if op == 1
         system ('cls||clear')
         cep()
     end
-    if op == 2 || 02
+    if op == 2
         system ('cls||clear')
         ip()
     end
-    if op == 3 || 03
+    if op == 3
         system ('cls||clear')
         cnpj()
     end
-    if op == 4 || 04
+    if op == 4
         system ('cls||clear')
         cepv2()
     end
-    if op == 5 || 05
+    if op == 5
         system ('cls||clear')
         corretoras()
     end
-    if op == 6 || 06
+    if op == 6
         menuCptec()
     end
-    if op == 00 || 0
+    if op == 0
         exit()
 end
