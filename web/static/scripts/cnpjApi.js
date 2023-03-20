dd6cf90cbabbb1da668ee2889fd86ac1bb1ba191
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 09:23:48 -0300

    alterando de innerHTML para um laco for que executa uma lista de objetos

--- web/static/scripts/cnpjApi.js
+++ web/static/scripts/cnpjApi.js
@@ -7,24 +7,17 @@ if(btn){
     btn.onclick = ()=>{
         fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj.value}`).then(function(resp){
             resp.json().then(function(data){
-                ret.innerHTML=`
-                <div class='returnBusca'>
-                    <div class='cnpj'>
+                for(let [key, value] of Object.entries(data)){
+                    ret.innerHTML+=`
+                    <div class='${key}'>
                         <code>
-                            data_sitacao => ${data.data_situacao}
-                            fantasia => ${data.fantasia}
-                            complemento => ${data.complemento}
-                            tipo => ${data.tipo}
-                            nome => ${data.nome}
-                            efr => ${data.erf}
-                            abertura => ${data.abertura}
-                            telefone => ${data.telefone}
-                            email => ${data.email}
+                            ${key} => ${value}
                         </code>
                     </div>
-                </div>
-                `
-                console.log(data)
+                    `
+                    console.log(key, value)
+                }
+                
             })
         })
     }
