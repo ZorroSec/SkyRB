d460da13e465abe0a61431a008543aa21b11af16
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 11:35:18 -0300

    cnpj.rb terminado

--- app/commands/cnpj.rb
+++ app/commands/cnpj.rb
@@ -1,3 +1,12 @@
 require  'rest-client'
 require 'json'
 require_relative 'back.rb'
+def cnpj
+    print 'CNPJ > '
+    cnpj = gets.chomp
+    req = RestClient.get "https://brasilapi.com.br/api/cnpj/v1/#{cnpj}"
+    ret = JSON.parse(req.body)
+    ret.each do |key,value|
+        puts "#{key} => #{value}"
+    end
+end
