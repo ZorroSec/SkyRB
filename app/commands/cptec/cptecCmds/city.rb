998f13d990993619e5e23404215c04e7dbfcf322
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 18:43:38 -0300

    *

--- app/commands/cptec/cptecCmds/city.rb
+++ app/commands/cptec/cptecCmds/city.rb
@@ -1,1 +1,13 @@
-city.rb
+require 'rest-client'
+require 'json'
+require_relative '../../back.rb'
+def cityName
+	print 'CITY > '
+	city = gets.chomp
+	req = RestClient.get "https://brasilapi.com.br/api/cptec/v1/cidade/#{city}"
+	ret = JSON.parse(req.body)
+	ret.each do |key,value|
+		puts "#{key} => #{value}"
+	end
+	back()
+end
