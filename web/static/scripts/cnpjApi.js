b939310e740f47563f870cba145d16ec515027ef
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 09:12:40 -0300

    bug resolvido

--- web/static/scripts/cnpjApi.js
+++ web/static/scripts/cnpjApi.js
@@ -7,23 +7,23 @@ if(btn){
     btn.onclick = ()=>{
         fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj.value}`).then(function(resp){
             resp.json().then(function(data){
-                // ret.innerHTML=`
-                // <div class='returnBusca'>
-                //     <div class='cnpj'>
-                //         <code>
-                //             data_sitacao => ${data.data_situacao}
-                //             fantasia => ${data.fantasia}
-                //             complemento => ${data.complemento}
-                //             tipo => ${data.tipo}
-                //             nome => ${data.nome}
-                //             efr => ${data.erf}
-                //             abertura => ${data.abertura}
-                //             telefone => ${data.telefone}
-                //             email => ${data.email}
-                //         </code>
-                //     </div>
-                // </div>
-                // `
+                ret.innerHTML=`
+                <div class='returnBusca'>
+                    <div class='cnpj'>
+                        <code>
+                            data_sitacao => ${data.data_situacao}
+                            fantasia => ${data.fantasia}
+                            complemento => ${data.complemento}
+                            tipo => ${data.tipo}
+                            nome => ${data.nome}
+                            efr => ${data.erf}
+                            abertura => ${data.abertura}
+                            telefone => ${data.telefone}
+                            email => ${data.email}
+                        </code>
+                    </div>
+                </div>
+                `
                 console.log(data)
             })
         })
