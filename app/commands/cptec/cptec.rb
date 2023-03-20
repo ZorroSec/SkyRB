936a05d70af129a090b70527c2503c1aeea6f0ae
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 18:51:54 -0300

    ***

--- app/commands/cptec/cptec.rb
+++ app/commands/cptec/cptec.rb
@@ -1,3 +1,13 @@
 require 'rest-client'
 require 'json'
 require_relative '../back.rb'
+def menuCptec
+	menu = "
+	l---------------------------l
+	l     [ 01 ] City           l
+	l     [ 02 ] CLima          l
+	l---------------------------l
+	>>> "
+	print menu
+	opc = gets.chomp.to_i
+end
