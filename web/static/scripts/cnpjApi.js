64cf885c89921a217cbd97a7de8ff21d69aac769
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-20 09:10:40 -0300

    tentando resolver o erro do template cnpj

--- web/static/scripts/cnpjApi.js
+++ web/static/scripts/cnpjApi.js
@@ -5,27 +5,27 @@ const ret = document.getElementById('return')
 
 if(btn){
     btn.onclick = ()=>{
-        fetch('https://www.receitaws.com.br/v1/cnpj/'+cnpj.value, {mode:'no-cors',}).then(function(resp){
-            return resp.json()
-        }).then(function(data){
-            // ret.innerHTML=`
-            // <div class='returnBusca'>
-            //     <div class='cnpj'>
-            //         <code>
-            //             data_sitacao => ${data.data_situacao}
-            //             fantasia => ${data.fantasia}
-            //             complemento => ${data.complemento}
-            //             tipo => ${data.tipo}
-            //             nome => ${data.nome}
-            //             efr => ${data.erf}
-            //             abertura => ${data.abertura}
-            //             telefone => ${data.telefone}
-            //             email => ${data.email}
-            //         </code>
-            //     </div>
-            // </div>
-            // `
-            console.log(data)
+        fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj.value}`).then(function(resp){
+            resp.json().then(function(data){
+                // ret.innerHTML=`
+                // <div class='returnBusca'>
+                //     <div class='cnpj'>
+                //         <code>
+                //             data_sitacao => ${data.data_situacao}
+                //             fantasia => ${data.fantasia}
+                //             complemento => ${data.complemento}
+                //             tipo => ${data.tipo}
+                //             nome => ${data.nome}
+                //             efr => ${data.erf}
+                //             abertura => ${data.abertura}
+                //             telefone => ${data.telefone}
+                //             email => ${data.email}
+                //         </code>
+                //     </div>
+                // </div>
+                // `
+                console.log(data)
+            })
         })
     }
 }
